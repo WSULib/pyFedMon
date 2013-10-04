dada9f26bf253a1754c8f8cfa829294e25b303da
Cole Hudson <Cole.Hudson@wayne.edu>
2013-10-04 13:54:07 -0400

    updating README and adding instructions

---
+++ sensitive_EDITME.py
@@ -0,0 +1,7 @@
+#sensitive information for pyFedMon
+#First, rename this file to sensitive.py
+
+#Change username to fedora admin user
+username = "insert_name_here"
+#Change password to fedora admin user password
+password = "my_password_goes_here"
